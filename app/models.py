aeb935a5db965bc79a5248a3162775ba631075ca
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-04-29 09:21:05 +1200

    models.py updates

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
 from app.routes import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Colour(db.Model):
     __tablename__ = "Colour"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100))
     hex_code = db.Column(db.String(7), unique=True)
+
+class User(db.Model):
+    __tablename__ = "User"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    privilege = db.Column(db.Integer, default=0)  # 0 is normal user 1 is admin
