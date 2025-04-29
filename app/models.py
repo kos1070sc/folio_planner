74030bbbcc105885bfdbf7fb6ca47c7e04467b54
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-04-29 16:44:42 +1200

    started working on login system

--- app/models.py
+++ app/models.py
@@ -13,3 +13,15 @@ class User(db.Model):
     name = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     privilege = db.Column(db.Integer, default=0)  # 0 is normal user, 1 is admin
+
+def set_password(self, password):
+    self.password = generate_password_hash(password, method='pbkdf2:sha256', salt_length = 8)
+    #This function hashes the password before storing it
+    #It uses PBKDF2-SHA256 password hasher with 8 byte salt 
+    #This means that the hashed password will always be 60 characters long
+
+def check_password(self, password):
+    return check_password_hash(self.password, password)
+    #This compares the password to the stored hash to check if a password is correct
+    #If it mathes it will return True if not then False 
+
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,17 @@
 from app import app
-from flask import render_template, abort, request
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
+import secrets
+import app.models as models
+app.secret_key = secrets.token_hex(32)
+# Creates a random key that is 64 characters long to encrypt session data
+# This makes more secure and protects against seesion hijacking 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db = SQLAlchemy()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, "folio.db")
 db.init_app(app)
-
-import app.models as models
 
 @app.route('/')
 def root():
@@ -19,3 +22,31 @@ def root():
 def colour():
     colours = models.Colour.query.all()
     return render_template("colour.html", colours=colours)
+
+@app.route('/create_account')
+def create_account():
+    #create account page
+    if request.method == 'POST':
+        username = request.form('username')
+        password = request.form('password')
+        #gets username and password from html form
+
+        if models.User.query.filter_by(name = username).first():
+        #queries the database and look for the first match 
+        #if not matches it will return None
+            flash('Username already exists')
+            return redirect(url_for('login'))
+            #if username matches redirects the user to login instead
+
+        new_user = models.User(name = username, privilege = 0)
+        new_user.set_password(password)
+        #creates a new user with no admin privileges and stores the hashed password
+        db.session.add(new_user)
+        db.session.commit()
+        #commit to the database
+        #.add prepares the data 
+        #.commit actually saves it 
+
+        flash('Account created! Please login.')
+        return redirect(url_for('login'))
+    return render_template('create_account.html')
